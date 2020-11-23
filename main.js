abcdcd4724a21748d904734b316a184fc3f1f61d
Andrea Wu <lixianhyun@gmail.com>
2020-11-22 17:52:37 -0800

    get 'label' & 'input' elements to JS file and add eventListener to checkBoxes

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 window.onload = () => {
-    
+
+    const checkBoxes = document.getElementsByClassName("checkBox");
+    const labels = document.getElementsByTagName("label");
+
+    for(let i = 0; i < checkBoxes.length; i++){
+        checkBoxes[i].addEventListener("click", function() {
+            labels[i].style.textDecoration = "line-through";
+        })
+    }
+
 }
