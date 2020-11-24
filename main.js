31523c839f3327653d65fe19618256bba0bd72b7
Andrea Wu <lixianhyun@gmail.com>
2020-11-23 23:27:28 -0800

    add 'countCkick' array

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 window.onload = () => {
 
+    let countClick = 0;
+
+    const contents = document.getElementsByClassName("contents");
     const checkBoxes = document.getElementsByClassName("checkBox");
     const labels = document.getElementsByTagName("label");
 
     for(let i = 0; i < checkBoxes.length; i++){
         checkBoxes[i].addEventListener("click", function() {
-            labels[i].style.textDecoration = "line-through";
-        })
+            labels[i].classList.toggle("checked");
+            contents[i].classList.toggle("checked");
+            countClick++;
+            console.log(countClick);
+        });
     }
+   
+
 
 }
