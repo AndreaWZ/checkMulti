1a38dc585cf7bbc8f8d249ba4cb641a40514885e
Andrea Wu <lixianhyun@gmail.com>
2020-11-29 12:25:07 -0800

    replace 'toggle' to 'add' in classList atribute

--- main.js
+++ main.js
@@ -12,20 +12,23 @@ window.onload = () => {
                     clicks.push(i);
                     if(clicks[0] < clicks[1]){ // increase
                         for(let i = clicks[0]; i <= clicks[1]; i++){
-                            labels[i].classList.toggle("checked");
-                            contents[i].classList.toggle("checked");
+                            labels[i].classList.add("checked");
+                            contents[i].classList.add("checked");
                             checkBoxes[i].setAttribute("checked", "true");
                         }
                     } else if(clicks[0] > clicks[1]){ // decrease
                         for(let i = clicks[1]; i <= clicks[0]; i++){
-                            labels[i].classList.toggle("checked");
-                            contents[i].classList.toggle("checked");
+                            labels[i].classList.add("checked");
+                            contents[i].classList.add("checked");
                             checkBoxes[i].setAttribute("checked", "true");
                         }
                     }
                 }
             } else { // zero elememts in array
                 clicks.push(i);
+                labels[i].classList.add("checked");
+                contents[i].classList.add("checked");
+                checkBoxes[i].setAttribute("checked", "true");
             }
         });
     }
