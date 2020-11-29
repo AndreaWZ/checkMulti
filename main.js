77d5b92586ddeac9c930bfe865d52a272460afb8
Andrea Wu <lixianhyun@gmail.com>
2020-11-28 23:14:11 -0800

    add 'clicks' array

--- main.js
+++ main.js
@@ -3,14 +3,15 @@ window.onload = () => {
     const contents = document.getElementsByClassName("contents");
     const checkBoxes = document.getElementsByClassName("checkBox");
     const labels = document.getElementsByTagName("label");
+    const clicks = [];
 
     for(let i = 0; i < checkBoxes.length; i++){
         checkBoxes[i].addEventListener("click", function() {
-            labels[i].classList.toggle("checked");
-            contents[i].classList.toggle("checked");
+            clicks.push(i);
+            console.log(clicks);
+            // labels[i].classList.toggle("checked");
+            // contents[i].classList.toggle("checked");
         });
     }
-   
-
 
 }
